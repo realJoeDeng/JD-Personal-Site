3fc226b991dbf620e160fdcda55d62feed39c139
realJoeDeng <realjoedengigo@gmail.com>
2026-06-16 16:37:49 +0800

    Apply original-aspect Life reel layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Component from './App';
 import './index.css';
 import './aboutPatch';
+import './lifeReelPatch';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
